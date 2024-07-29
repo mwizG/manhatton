5f2cf7b4bc4f9207aac7374485cba947f56142c4
mwiza <mwiza0979001175@gmail.com>
2024-07-29 14:08:30 +0200

    working on logbook

--- ireen/logbook/urls.py
+++ ireen/logbook/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
 
-app_name='logbook'
+app_name = 'logbook'
+
 urlpatterns = [
     path('', views.logbook_home, name='logbook'),
     path('add_expense/', views.add_expense, name='add_expense'),
+    path('update_expense/<int:pk>/', views.update_expense, name='update_expense'),
+    path('delete_expense/<int:pk>/', views.delete_expense, name='delete_expense'),
     path('add_sale/', views.add_sale, name='add_sale'),
+    path('update_sale/<int:pk>/', views.update_sale, name='update_sale'),
+    path('delete_sale/<int:pk>/', views.delete_sale, name='delete_sale'),
 ]
--- ireen/logbook/views.py
+++ ireen/logbook/views.py
@@ -1,13 +1,41 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Expense, Sale, Tip
+from django.core.serializers.json import DjangoJSONEncoder
+from django.http import JsonResponse
+from .models import Expense, Sale
 from .forms import ExpenseForm, SaleForm
+import json
+
 
 @login_required
 def logbook_home(request):
     expenses = Expense.objects.filter(user=request.user)
     sales = Sale.objects.filter(user=request.user)
-    return render(request, 'logbook/index.html', {'expenses': expenses, 'sales': sales})
+
+    # Calculate total spent and total earned
+    total_spent = sum(expense.amount_spent for expense in expenses)
+    total_earned = sum(sale.amount_earned for sale in sales)
+    profit = total_earned - total_spent
+
+    # Format values with currency symbol
+    formatted_total_spent = f'k {total_spent:,.2f}'
+    formatted_total_earned = f'K{total_earned:,.2f}'
+    formatted_profit = f'K{profit:,.2f}'
+    
+    print('formatted:', formatted_profit)
+    # Serialize data for Chart.js
+    expense_data = json.dumps(list(expenses.values('date', 'amount_spent')), cls=DjangoJSONEncoder)
+    sale_data = json.dumps(list(sales.values('date', 'amount_earned')), cls=DjangoJSONEncoder)
+
+    return render(request, 'logbook/index.html', {
+        'expenses': expenses,
+        'sales': sales,
+        'expense_data': expense_data,
+        'sale_data': sale_data,
+        'total_spent': formatted_total_spent,
+        'total_earned': formatted_total_earned,
+        'profit': formatted_profit,
+    })
 
 @login_required
 def add_expense(request):
@@ -23,6 +51,26 @@ def add_expense(request):
     return render(request, 'logbook/add_expense.html', {'form': form})
 
 @login_required
+def update_expense(request, pk):
+    expense = Expense.objects.get(pk=pk, user=request.user)
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST, instance=expense)
+        if form.is_valid():
+            form.save()
+            return redirect('logbook:logbook')
+    else:
+        form = ExpenseForm(instance=expense)
+    return render(request, 'logbook/add_expense.html', {'form': form})
+
+@login_required
+def delete_expense(request, pk):
+    expense = Expense.objects.get(pk=pk, user=request.user)
+    if request.method == 'POST':
+        expense.delete()
+        return redirect('logbook:logbook')
+    return render(request, 'logbook/delete_expense.html', {'expense': expense})
+
+@login_required
 def add_sale(request):
     if request.method == 'POST':
         form = SaleForm(request.POST)
@@ -34,3 +82,23 @@ def add_sale(request):
     else:
         form = SaleForm()
     return render(request, 'logbook/add_sale.html', {'form': form})
+
+@login_required
+def update_sale(request, pk):
+    sale = Sale.objects.get(pk=pk, user=request.user)
+    if request.method == 'POST':
+        form = SaleForm(request.POST, instance=sale)
+        if form.is_valid():
+            form.save()
+            return redirect('logbook:logbook')
+    else:
+        form = SaleForm(instance=sale)
+    return render(request, 'logbook/add_sale.html', {'form': form})
+
+@login_required
+def delete_sale(request, pk):
+    sale = Sale.objects.get(pk=pk, user=request.user)
+    if request.method == 'POST':
+        sale.delete()
+        return redirect('logbook:logbook')
+    return render(request, 'logbook/delete_sale.html', {'sale': sale})
